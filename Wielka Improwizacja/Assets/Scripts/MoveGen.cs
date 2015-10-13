74ded5f681a0cc21c0ff8f1e08cb9bc2972b3d82
s76 <sheepw76@gmail.com>
2015-10-14 01:01:26 +0200

    ....

---
+++ Wielka Improwizacja/Assets/Scripts/MoveGen.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public enum MOVE { UP , DOWN , LEFT, RIGHT }
+
+public class MoveGen : MonoBehaviour {
+	public Sprite up,down,left,right;
+	public Image[] arrows;
+	public float genInterval;
+
+	static MOVE[] tableMut = { MOVE.UP , MOVE.DOWN , MOVE.LEFT, MOVE.RIGHT };
+
+	public MOVE[] moves;//  { get; private set; } 
+	public MOVE currentMove { 
+		get {
+			return moves[currentIndex];
+		}
+	} 
+
+	int currentIndex;
+
+	void Awake (){ 
+		GenMove(arrows.Length);
+	}
+
+	IEnumerator _gen;
+
+	public void StartGen () {
+		if ( _gen == null ) 
+			StartCoroutine( _gen = Gen() );
+	}
+
+	public void StopGen () {
+		if ( _gen != null ) {
+			StopCoroutine(_gen);
+			_gen = null;
+		}
+	}
+
+	void OnGUI () {
+		if ( GUILayout.Button("Start Gen") ) {
+			StartGen();
+		}
+		if ( GUILayout.Button("Stop Gen") ) {
+			StopGen();
+		}
+	}
+
+	IEnumerator Gen () {
+		while (true ) {
+			yield return new WaitForSeconds(genInterval);
+			GenMove(1);
+		}
+	}
+
+	void GenMove (int nb ) {
+		while ( nb > 0 ) {
+			moves[currentIndex = ( (currentIndex+1) % moves.Length )]  = tableMut[Random.Range(0,tableMut.Length)];
+			nb --;
+		}
+	}
+	
+}
--- Wielka Improwizacja/Assets/Scripts/PlayerController.cs
+++ Wielka Improwizacja/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,36 @@ public class PlayerController : MonoBehaviour
 {
     private PlayerInput _input;
     private float _speed = 4;
+
+	Animator animator;
+
     // Use this for initialization
     void Start()
     {
         _input = GetComponent<PlayerInput>();
+		animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(_input.Movement().x, 0, _input.Movement().y) * Time.deltaTime * _speed);
+		var _move = _input.Movement();
+		if ( _move.x != 0 | _move.y != 0 ) {
+			if ( animator != null ) animator.Play("move");
+		}
+		transform.Translate(new Vector3(_move.x, 0, _move.y) * Time.deltaTime * _speed);
         transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x, -6, 6), transform.localPosition.y, Mathf.Clamp(transform.localPosition.z, 0, 5));
-    }
+		if ( _input.SkillLeft() ) { 
+			if (    animator != null ) animator.Play("left");
+		}
+
+		if ( _input.SkillDown() ) {
+			if ( animator != null ) animator.Play("crouch");
+		}
+
+		if ( _input.SkillUp() ) {
+			Debug.Log("asda");
+			if ( animator != null ) animator.Play("jump");
+		}
+	}
 }

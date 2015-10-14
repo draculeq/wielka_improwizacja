626e987880e19738748a114c1565c49839e63a85
Draculeq <gsniecinski@gmail.com>
2015-10-14 11:28:09 +0200

    score ui

---
+++ Wielka Improwizacja/Assets/Scripts/UIScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIScore : MonoBehaviour
+{
+    [SerializeField]
+    private PointCounter _counter;
+    [SerializeField]
+    private Text _text;
+    // Use this for initialization
+    void Start()
+    {
+        _counter.PointAdded += _counter_PointAdded;
+        _text.text = "Score: 0";
+    }
+
+    private void _counter_PointAdded(int obj)
+    {
+        _text.text = "Score: " + obj;
+    }
+}

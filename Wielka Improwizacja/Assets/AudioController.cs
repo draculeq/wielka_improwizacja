f278322d7d362b47cc164753e0048747d570f12c
Draculeq <gsniecinski@gmail.com>
2015-10-15 00:12:31 +0200

    audio

--- Wielka Improwizacja/Assets/AudioController.cs
+++ Wielka Improwizacja/Assets/AudioController.cs
@@ -2,72 +2,88 @@
 using System.Collections;
 
 [RequireComponent(typeof(AudioSource))]
-public class AudioController : MonoBehaviour {
-	AudioSource au_source;
-	[SerializeField] AudioClip gwizd,slash,kompromitacja, wspaniale, mistrz, kombo, znakomicie,nieslychanie;
-	
-	void Awake () {
-		if ( GameObject.FindObjectsOfType (typeof(AudioController)).Length <= 1 ) {
-			au_source = GetComponent<AudioSource>();
-			DontDestroyOnLoad(gameObject);
-		} else DestroyObject(gameObject);
-	}	
+public class AudioController : MonoBehaviour
+{
+    AudioSource au_source;
+    [SerializeField]
+    AudioClip gwizd, slash, kompromitacja, wspaniale, mistrz, kombo, znakomicie, nieslychanie;
 
-	public void PomidorCrashed () {
-		au_source.PlayOneShot(slash,0.05f);
-	}
-    
-	public void KwiatekCaptured (){
-		au_source.PlayOneShot(gwizd,0.02f);
-	}
+    void Awake()
+    {
+        if (GameObject.FindObjectsOfType(typeof(AudioController)).Length <= 1)
+        {
+            au_source = GetComponent<AudioSource>();
+            DontDestroyOnLoad(gameObject);
+        }
+        else DestroyObject(gameObject);
+    }
 
-	public void BothFailed () {
-		au_source.PlayOneShot(kompromitacja);
-	}
+    public void PomidorCrashed()
+    {
+        au_source.PlayOneShot(slash, 0.05f);
+    }
 
-	public void RandomComboSound () {
-		int r = Random.Range(0,4);
-		if ( r == 0) au_source.PlayOneShot(kombo);
-		if ( r == 1)  au_source.PlayOneShot(wspaniale);
-		if ( r == 2) au_source.PlayOneShot(mistrz);
-		if ( r == 3) au_source.PlayOneShot(nieslychanie);
-		
-	}
+    public void KwiatekCaptured()
+    {
+        au_source.PlayOneShot(gwizd, 0.02f);
+    }
 
-	public void Win () {
-		
-		StartCoroutine(Win2() );
-	}
+    public void BothFailed()
+    {
+        au_source.PlayOneShot(kompromitacja);
+    }
 
-	IEnumerator Win2 () {
-		au_source.PlayOneShot(znakomicie);
-		yield return new WaitForSeconds(1f);
-		au_source.PlayOneShot(znakomicie);
-	}
-	public void MegaWin () {
-		StartCoroutine(MegaWin2() );
-	}
+    public void RandomComboSound()
+    {
+        int r = Random.Range(0, 4);
+        if (r == 0) au_source.PlayOneShot(kombo);
+        if (r == 1) au_source.PlayOneShot(wspaniale);
+        if (r == 2) au_source.PlayOneShot(mistrz);
+        if (r == 3) au_source.PlayOneShot(nieslychanie);
 
-	IEnumerator MegaWin2 () {
-		au_source.PlayOneShot(znakomicie);
-		yield return new WaitForSeconds(0.5f);
-		au_source.PlayOneShot(znakomicie);
-		yield return new WaitForSeconds(0.5f);
-		au_source.PlayOneShot(znakomicie);
-		yield return new WaitForSeconds(0.5f);
-		au_source.PlayOneShot(znakomicie);
+    }
 
-	}
+    public void Win()
+    {
 
-	#region hide
-	private static AudioController _instance;
-	public static AudioController Instance {
-		get {
-			if ( _instance == null ) {
-				_instance = GameObject.FindObjectOfType(typeof(AudioController)) as AudioController;
-			}
-			return _instance;
-		}
-	}
-	#endregion
+        StartCoroutine(Win2());
+    }
+
+    IEnumerator Win2()
+    {
+        au_source.PlayOneShot(znakomicie);
+        yield return new WaitForSeconds(1f);
+        au_source.PlayOneShot(znakomicie);
+    }
+    public void MegaWin()
+    {
+        StartCoroutine(MegaWin2());
+    }
+
+    IEnumerator MegaWin2()
+    {
+        au_source.PlayOneShot(znakomicie);
+        yield return new WaitForSeconds(0.5f);
+        au_source.PlayOneShot(znakomicie);
+        yield return new WaitForSeconds(0.5f);
+        au_source.PlayOneShot(znakomicie);
+        yield return new WaitForSeconds(0.5f);
+        au_source.PlayOneShot(znakomicie);
+
+    }
+
+    #region hide
+    private static AudioController _instance;
+    public static AudioController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType(typeof(AudioController)) as AudioController;
+            }
+            return _instance;
+        }
+    }
+    #endregion
 }
--- Wielka Improwizacja/Assets/Scripts/PlayerController.cs
+++ Wielka Improwizacja/Assets/Scripts/PlayerController.cs
@@ -35,47 +35,53 @@ public class PlayerController : MonoBehaviour
 
     private void Animations(Vector2 _move)
     {
-       
-
         if (_input.SkillLeft())
         {
             if (animator != null) animator.Play("left");
-        } else if (_input.SkillRight())
-		{
-			if (animator != null) animator.Play("right");
-		} else if (_input.SkillDown())
+        }
+        else if (_input.SkillRight())
+        {
+            if (animator != null) animator.Play("right");
+        }
+        else if (_input.SkillDown())
         {
             if (animator != null) animator.Play("crouch");
-        } else if (_input.SkillUp())
+        }
+        else if (_input.SkillUp())
         {
             if (animator != null) animator.Play("jump");
-		} else if (_move.x != 0 | _move.y != 0)
-		{
+        }
+        else if (_move.x != 0 | _move.y != 0)
+        {
 
-			if (animator != null) {
-				if ( animator.GetCurrentAnimatorStateInfo(0).IsName("idle") ) 
-					animator.Play("move");
-			}
-		}
+            if (animator != null)
+            {
+                if (animator.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+                    animator.Play("move");
+            }
+        }
     }
 
-	
-	int success_combo_count = 0;
-	int next_combo = 4;
 
-	void ComboCount () {
-		success_combo_count ++;
-		Debug.Log("success_combo_count = "+ success_combo_count + " next_combo="+next_combo);
-		if ( success_combo_count > next_combo) {
-			AudioController.Instance.RandomComboSound();
-			next_combo += 3;
-		}
-	}
+    int success_combo_count = 0;
+    int next_combo = 4;
 
-	void ComboFail () {
-		success_combo_count = 0;
-		next_combo = 4;
-	}
+    void ComboCount()
+    {
+        success_combo_count++;
+        Debug.Log("success_combo_count = " + success_combo_count + " next_combo=" + next_combo);
+        if (success_combo_count > next_combo)
+        {
+            AudioController.Instance.RandomComboSound();
+            next_combo += 3;
+        }
+    }
+
+    void ComboFail()
+    {
+        success_combo_count = 0;
+        next_combo = 4;
+    }
 
     private void Skills()
     {
@@ -84,52 +90,52 @@ public class PlayerController : MonoBehaviour
         if (currentMove == null)
         {
             return;
-		}
-		currentMove.onFailCallBack = ComboFail;
+        }
+        currentMove.onFailCallBack = ComboFail;
 
         if (_input.SkillLeft())
         {
             if (currentMove.Check(MOVE.LEFT))
-			{ 
-				ComboCount(); 
-			}
-            else 
-			{
-				currentMove.Fail();
-			}
+            {
+                ComboCount();
+            }
+            else
+            {
+                currentMove.Fail();
+            }
         }
         if (_input.SkillRight())
         {
             if (currentMove.Check(MOVE.RIGHT))
             {
-				ComboCount(); 
-			}
-			else 
-			{
-				currentMove.Fail();
-			}
+                ComboCount();
+            }
+            else
+            {
+                currentMove.Fail();
+            }
         }
         if (_input.SkillUp())
         {
             if (currentMove.Check(MOVE.UP))
             {
-				ComboCount(); 
-			}
-			else 
-			{
-				currentMove.Fail();
-			}
+                ComboCount();
+            }
+            else
+            {
+                currentMove.Fail();
+            }
         }
         if (_input.SkillDown())
         {
             if (currentMove.Check(MOVE.DOWN))
             {
-				ComboCount(); 
-			}
-			else 
-			{
-				currentMove.Fail();
-			}
+                ComboCount();
+            }
+            else
+            {
+                currentMove.Fail();
+            }
         }
     }
 }

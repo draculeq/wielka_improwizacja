e10c3c0e1bf0eb9311b550d74c8cab60877840d1
Draculeq <gsniecinski@gmail.com>
2015-10-14 20:37:01 +0200

    particle satisfaction

--- Wielka Improwizacja/Assets/UISatisfaction.cs
+++ Wielka Improwizacja/Assets/UISatisfaction.cs
@@ -7,6 +7,7 @@ public class UISatisfaction : MonoBehaviour
     public Image fill;
     public Color green;
     public Color red;
+    public ParticleSystem particle;
     // Use this for initialization
     void Start()
     {
@@ -16,6 +17,17 @@ public class UISatisfaction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (AudienceSatisfaction.Instance.Satisfaction >= 100 && (particle.isStopped || particle.isPaused))
+        {
+            Debug.Log("play");
+            particle.Play();
+        }
+        else if (particle.isPlaying && AudienceSatisfaction.Instance.Satisfaction < 100)
+        {
+            Debug.Log("stop");
+            particle.Stop();
+        }
+
         fill.fillAmount = AudienceSatisfaction.Instance.Satisfaction / 100;
         fill.color = Color.Lerp(green, red, 1 - AudienceSatisfaction.Instance.Satisfaction / 100);
     }

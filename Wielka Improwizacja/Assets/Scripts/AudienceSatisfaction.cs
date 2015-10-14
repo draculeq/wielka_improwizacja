8392bd53907317cf0dd0c80b73f98de892c35483
Draculeq <gsniecinski@gmail.com>
2015-10-14 18:41:21 +0200

    wictory & lose

--- Wielka Improwizacja/Assets/Scripts/AudienceSatisfaction.cs
+++ Wielka Improwizacja/Assets/Scripts/AudienceSatisfaction.cs
@@ -7,6 +7,7 @@ public class AudienceSatisfaction : MonoBehaviour
     public float Speed;
     public float punishment;
     public float reward;
+    public float winTimer;
     // Use this for initialization
     void Awake()
     {
@@ -17,17 +18,35 @@ public class AudienceSatisfaction : MonoBehaviour
     void Update()
     {
         Satisfaction -= Speed * Time.deltaTime;
-        if (Satisfaction<0)
+        if (Satisfaction < 0)
+        {
             Debug.Log("GAME OVER");
+            enabled = false;
+        }
+
+        if (Satisfaction < 100)
+            winTimer = -1;
+        if (winTimer == -1 && Satisfaction > 100)
+            winTimer = 0;
+        if (winTimer >= 0 && Satisfaction > 100)
+            winTimer += Time.deltaTime;
+        if (winTimer >= 10)
+        {
+            enabled = false;
+            Debug.Log("Wictory");
+        }
     }
 
     public void Good()
     {
+        if (enabled == false) return;
         Satisfaction += reward;
+        Satisfaction = Mathf.Clamp(Satisfaction, -1, 110);
     }
 
     public void Bad()
     {
+        if (enabled == false) return;
         Satisfaction -= punishment;
     }
 }

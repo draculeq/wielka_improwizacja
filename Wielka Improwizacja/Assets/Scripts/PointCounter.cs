df98674c404d1589217042524d17e846336163ca
s76 <sheepw76@gmail.com>
2015-10-14 16:00:52 +0200

    fix2

--- Wielka Improwizacja/Assets/Scripts/PointCounter.cs
+++ Wielka Improwizacja/Assets/Scripts/PointCounter.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 public class PointCounter : MonoBehaviour
 {
 	public bool usingLeftPanel;
-	[SerializeField] UIScore ui_score;
 	int totalPoints;
-	PlayerController contro;
 
 	void Awake () {
 		totalPoints=0;
-		contro = GetComponent<PlayerController>();
 	}
 	public void AddPoint (int points ) {
 		totalPoints += points;
